e5e08adcd049839eff9ddf996a8800ba2c513ce9
AppSeed Generated Projects <108474994+appseed-projects3@users.noreply.github.com>
2023-04-16 17:53:52 +0300

    initial commit

---
+++ core/urls.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+
+try:
+    from rest_framework.authtoken.views import obtain_auth_token
+except:
+    pass
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("admin_volt.urls")),
+    path("login/jwt/", view=obtain_auth_token),
+]
+
+try:
+    urlpatterns.append( path("api/", include("api.urls")) )
+except:
+    pass
